f774e9b53d159867512d07a8f35d63dcc5efff82
Renato Costa 55 <renatocosta55sp@gmail.com>
2025-01-10 20:48:27 -0300

    Update

---
+++ eventsourcing/event_store.go
@@ -0,0 +1,15 @@
+package eventsourcing
+
+import (
+	"context"
+	"errors"
+
+	"github.com/renatocosta55sp/modeling/domain"
+)
+
+var ErrConcurrencyConflict = errors.New("concurrency conflict: event version mismatch")
+
+type EventStore interface {
+	AppendToStream(ctx context.Context, streamId string, newEvents []domain.Event, expectedVersion int) error
+	ReadAllStream(ctx context.Context, streamID string) ([]domain.Event, error)
+}
