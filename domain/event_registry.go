5028d7bcd9e45c3681badcde1cb412716be4ffd7
Renato Costa 55 <renatocosta55sp@gmail.com>
2025-02-04 11:02:11 -0300

    Update

---
+++ domain/event_registry.go
@@ -0,0 +1,47 @@
+package domain
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
+// EventRegistry is a component responsible for managing event types and creating event instances.
+type EventRegistry struct {
+	typeMap map[string]reflect.Type
+}
+
+func NewEventRegistry() *EventRegistry {
+	return &EventRegistry{
+		typeMap: make(map[string]reflect.Type),
+	}
+}
+
+func (r *EventRegistry) RegisterEvents(events map[string]reflect.Type) {
+	for eventName, eventType := range events {
+		r.typeMap[eventName] = eventType
+	}
+}
+
+// CreateEvent creates a new instance of the event based on the event type and unmarshals the payload.
+func (r *EventRegistry) CreateEvent(eventType string, payload []byte) (Event, error) {
+	eventTypeInstance, ok := r.typeMap[eventType]
+	if !ok {
+		return nil, fmt.Errorf("unknown event type: %s", eventType)
+	}
+
+	// Create a new instance of the event type
+	event := reflect.New(eventTypeInstance).Interface()
+
+	// Unmarshal the jsonb payload into the event instance
+	if err := json.Unmarshal(payload, event); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal event payload: %w", err)
+	}
+
+	// Cast the event to domain.Event
+	if domainEvent, ok := event.(Event); ok {
+		return domainEvent, nil
+	}
+
+	return nil, fmt.Errorf("event does not implement domain.Event interface")
+}
