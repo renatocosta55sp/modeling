986c2593cec152cbb481686d2bf784d770059141
Renato Costa 55 <renatocosta55sp@gmail.com>
2025-02-06 12:25:14 -0300

    Update

--- eventstore/event_store.go
+++ eventstore/event_store.go
@@ -10,7 +10,7 @@ import (
 var ErrConcurrencyConflict = errors.New("concurrency conflict: event version mismatch")
 
 type EventStore interface {
-	AppendToStream(ctx context.Context, streamId string, newEvents []domain.Event, aggregateVersion int) error
+	AppendToStream(ctx context.Context, streamId string, newEvents []domain.Event, expectedVersion int) error
 	ReadStream(ctx context.Context, streamID string) ([]domain.Event, error)
 	ReadAllStream(ctx context.Context) ([]domain.Event, error)
 }
