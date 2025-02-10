3f91e63d422abb5f4dfd13d3850c06fb46e7a3f3
Renato Costa 55 <renatocosta55sp@gmail.com>
2025-02-10 13:28:45 -0300

    Update

--- eventstore/snapshot.go
+++ eventstore/snapshot.go
@@ -16,5 +16,5 @@ func ShouldTakeSnapshot(agg *domain.Aggregate, frequency int) bool {
 
 type SnapshotStore interface {
 	WriteSnapshot(ctx context.Context, streamId string, event domain.Event, version int) error
-	ReadSnapshot(ctx context.Context, streamID string) (domain.Event, error)
+	ReadSnapshot(ctx context.Context, streamID string) (int, error)
 }
