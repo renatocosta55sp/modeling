76d2d1c4f811a22182f91b8be91a2a4395a66005
Renato Costa 55 <renatocosta55sp@gmail.com>
2025-02-10 12:24:26 -0300

    Update

--- eventstore/snapshot.go
+++ eventstore/snapshot.go
@@ -6,7 +6,7 @@ import (
 	"github.com/renatocosta55sp/modeling/domain"
 )
 
-func ShouldTakeSnapshot(agg domain.Aggregate, frequency int) bool {
+func ShouldTakeSnapshot(agg *domain.Aggregate, frequency int) bool {
 	if frequency <= 0 {
 		// Invalid frequency, default to not taking snapshots
 		return false
